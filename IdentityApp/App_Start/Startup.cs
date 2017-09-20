d321928909953929bc725fbf81c21ab9cef2b2a4
Unknown <pavel1yakimovich@gmail.com>
2017-09-20 16:21:59 +0300

    configuration and registration added

---
+++ IdentityApp/App_Start/Startup.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Owin;
+using Owin;
+using IdentityApp.Models;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.AspNet.Identity;
+
+[assembly: OwinStartup(typeof(IdentityApp.Startup))]
+
+namespace IdentityApp
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.CreatePerOwinContext<ApplicationContext>(ApplicationContext.Create);
+            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login")
+            });
+        }
+    }
+}
---
+++ IdentityApp/Models/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IdentityApp.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public int Year { get; set; }
+
+        public ApplicationUser() { }
+    }
+}
